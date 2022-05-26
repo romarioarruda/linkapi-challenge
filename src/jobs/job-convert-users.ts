bd2ce6f44923010228afaa0d079ca76fb22a4b70
Romário Arruda <romarioarruda98@gmail.com>
2022-05-26 16:16:37 -0300

    feat(job-convert-users): processo de obter dados dos usuário
    obtem os dados e armazena em memória

--- src/jobs/job-convert-users.ts
+++ src/jobs/job-convert-users.ts
@@ -2,9 +2,10 @@ import schedule from 'node-schedule'
 import { DiasDaSemana } from '../enums/days-of-week'
 import { MesesDoAno } from '../enums/months-of-year'
 import { HorasDoDia } from '../enums/hours-of-day'
-import LinkApiDesafioTech from '../clients/linkapi/linkapi-desafio-tech'
+import LinkApiUsers from '../clients/linkapi/linkapi-users'
+import { Users } from '../domain/Users'
 
-const MINUTE = '*/1' // */1 => a cada 1 minuto
+const MINUTE = '*/5' //  => roda a cada 5 minutos
 const HOUR = HorasDoDia.TODAS
 const DAY_OF_MONTH = '*'
 const MONTH = MesesDoAno.TODOS
@@ -12,15 +13,32 @@ const DAY_OF_WEEK = DiasDaSemana.TODOS
 
 const TIMER = `${MINUTE} ${HOUR} ${DAY_OF_MONTH} ${MONTH} ${DAY_OF_WEEK}`
 
-const api = new LinkApiDesafioTech()
+const api = new LinkApiUsers()
 
-async function runJob() {
+async function runJob(): Promise<void> {
   console.log('Iniciando busca por usuários')
-  const users = await api.getUsers()
+  const users = new Users()
+  const apiUserList = await api.getUsers()
 
-  for (let user of users) {
-    console.log(user)
+  for (let user of apiUserList) {
+    const userId = Number(user.id._text)
+    console.log(`User: ${userId}`)
+    const [address, contact] = await Promise.all([
+      api.getUserAddress(userId),
+      api.getUserContact(userId),
+    ])
+
+    users.push(
+      userId,
+      `${user.firstName._text} ${user.lastName._text}`,
+      user.email._text,
+      address.street._text,
+      Number(address.number._text),
+      contact.phoneNumber._text,
+    )
   }
+
+  console.log('USERS: ', users.lista)
 }
 
 export default function jobConvertUsers(): void {
