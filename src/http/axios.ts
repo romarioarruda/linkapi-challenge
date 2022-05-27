78cb0db41f496b10c1ad0d9682a1c82caebdfd8d
Romário Arruda <romarioarruda98@gmail.com>
2022-05-27 15:30:43 -0300

    refactor(http): timeout
    add tempo limite de espera

--- src/http/axios.ts
+++ src/http/axios.ts
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const http = axios.create({
   baseURL: `${process.env.API_URL}`,
+  timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
     Authorization: `${process.env.API_TOKEN}`,
