1abf9278af98d5cb64ef0f9b43a1d671129d5d9c
Romário Arruda <romarioarruda98@gmail.com>
2022-05-27 15:35:48 -0300

    feat(index): carregando configuracao do mongo

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+import { mongoOpenConnection } from './config/mongodb'
 import jobConvertUsers from './jobs/job-convert-users'
+
+mongoOpenConnection()
 
 jobConvertUsers()
