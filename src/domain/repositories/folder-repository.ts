bf8f208f3e522a97ac4b8087dea268a934cbf624
Romário Arruda <romarioarruda98@gmail.com>
2022-05-28 13:30:56 -0300

    feat(domain): add metodo na interface FolderRepository

--- src/domain/repositories/folder-repository.ts
+++ src/domain/repositories/folder-repository.ts
@@ -2,5 +2,6 @@ import { IFolder } from '../../interfaces/IFolder'
 
 export interface FolderRepository {
   exists(name: string): Promise<boolean>
+  findMany(): Promise<IFolder[]>
   insertOne(folder: IFolder): Promise<boolean>
 }
