c5379e0d2a58aa38d11bda64643e4f362417c9ce
Romário Arruda <romarioarruda98@gmail.com>
2022-06-02 11:39:44 -0300

    refactor(mongodb): maxPoolSize
    add pool maximo

--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose'
 
 export const mongoOpenConnection = async (): Promise<void> => {
-  await mongoose.connect(`${process.env.MONGO_HOST}/linkapi_challenge`)
+  await mongoose.connect(`${process.env.MONGO_HOST}/linkapi_challenge`, {
+    maxPoolSize: 10,
+  })
 }
 
 export const mongoCloseConnection = async (): Promise<void> => {
